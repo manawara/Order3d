170b8340479f224e113371870d0ac45dc25d9384
manawara <marcinnawara@int.pl>
2024-08-17 19:05:53 +0200

    Configure project

---
+++ app/fonts.ts
@@ -0,0 +1,12 @@
+import { Montserrat, Lato, Manrope } from 'next/font/google'
+
+export const montserrat = Montserrat({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const manrope = Manrope({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+})
