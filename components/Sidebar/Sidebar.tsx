5ac9492874ea52be452cd0641760e9159645a724
manawara <marcinnawara@int.pl>
2024-08-25 22:35:38 +0200

    Create Sidebar component

---
+++ components/Sidebar/Sidebar.tsx
@@ -0,0 +1,18 @@
+'use client'
+import { useContextHamburger } from '@/context/ContextHamburger'
+import SidebarList from './SidebarList'
+const Sidebar = () => {
+  const ctx = useContextHamburger()
+
+  return (
+    <aside
+      className={`${
+        ctx.open ? 'translate-x-0' : '-translate-x-full'
+      } fixed  bg-greenDark z-10 duration-300 md:translate-x-0 min-w-48 p-4 border-r-[1px] border-greenLight min-h-screen pt-4`}
+    >
+      <SidebarList />
+    </aside>
+  )
+}
+
+export default Sidebar
---
+++ components/Sidebar/SidebarList.tsx
@@ -0,0 +1,56 @@
+'use client'
+import { Box, ShoppingCart, Users } from 'lucide-react'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+import { usePathname } from 'next/navigation'
+
+const SidebarList = () => {
+  const pathName = usePathname()
+  const slug = pathName.split('/').pop()
+
+  const listItems = [
+    {
+      href: '/dashboard/orders',
+      icon: <ShoppingCart size={20} strokeWidth={1} />,
+      text: 'Zamówienia',
+      active: slug === 'orders',
+    },
+    {
+      href: '/dashboard/products',
+      icon: <Box size={20} strokeWidth={1} />,
+      text: 'Produkty',
+      active: slug === 'products',
+    },
+    {
+      href: '/dashboard/users',
+      icon: <Users size={20} strokeWidth={1} />,
+      text: 'Użytkownicy',
+      active: slug === 'users',
+    },
+  ]
+
+  return (
+    <ul className="text-greenLight text-xs flex flex-col gap-2">
+      {listItems.map((item, index) => (
+        <motion.li
+          key={item.href}
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -20 }}
+          transition={{ duration: 0.3, delay: index * 0.1 }}
+        >
+          <Link
+            href={item.href}
+            className={`flex gap-2 items-center p-2 rounded-md w-full 
+              transition-colors duration-300 ease-in-out
+              ${item.active ? 'bg-green' : 'hover:bg-green'}`}
+          >
+            {item.icon} {item.text}
+          </Link>
+        </motion.li>
+      ))}
+    </ul>
+  )
+}
+
+export default SidebarList
