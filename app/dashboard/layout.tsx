44a15d478c269231fadf10fb45453bfe3dfb0cb6
manawara <marcinnawara@int.pl>
2024-08-25 22:33:45 +0200

    Move HamburgerProvider

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,18 @@
+'use client'
 import Header from '@/components/Header/Header'
+import Sidebar from '@/components/Sidebar/Sidebar'
+import HamburgerProvider from '@/context/ContextHamburger'
 import { ReactNode } from 'react'
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <>
+    <HamburgerProvider>
       <Header />
-      <main>{children}</main>
-    </>
+      <main className="flex">
+        <Sidebar />
+        <div className="flex-auto mx-4 md:ml-52 mt-2">{children}</div>
+      </main>
+    </HamburgerProvider>
   )
 }
 
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,15 +2,11 @@
 import Logo from '../Logo'
 import Profile from '../Profile/Profile'
 import Hamburger from './Hamburger'
-import HamburgerProvider from '@/context/ContextHamburger'
 
 const Header = () => {
   return (
-    <header className="border-b bottom-0 border-b-greenLight px-8 py-4 flex justify-between items-center">
-      <HamburgerProvider>
-        <Hamburger />
-      </HamburgerProvider>
-
+    <header className="border-b bottom-0 border-b-greenLight px-8 py-4 flex justify-between items-center z-20 ">
+      <Hamburger />
       <Logo />
       <Profile />
     </header>
