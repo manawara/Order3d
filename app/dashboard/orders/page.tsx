81c5d66a5be0b49592ea989c2ce38f7ef7fb4a0b
manawara <marcinnawara@int.pl>
2024-08-29 20:46:26 +0200

    Add new page orders

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -1,9 +1,10 @@
 import Card from '@/components/Card/Card'
+import Orders from '@/components/Orders/Orders'
 import React from 'react'
 
-const page = () => {
+const OrdersPage = () => {
   return (
-    <div className="max-w-screen-xl">
+    <div className="">
       <h2 className="text-2xl font-semibold my-4">Druk 3D zamówienia</h2>
       <p className="mt-2 text-s mb-8">Z łatwością zarządzaj zamówieniami druku 3D.</p>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
@@ -12,8 +13,9 @@ const page = () => {
         <Card title="Zrealizowane zamówienia" description="Pomyślnie wydrukowane zamówienia" count={12} />
         <Card title="Anulowane zamówienia" description="Zamówienia, które zostały anulowane" count={12} />
       </div>
+      <Orders />
     </div>
   )
 }
 
-export default page
+export default OrdersPage
---
+++ components/Orders/Orders.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Table from '../Table/Table'
+
+const Orders = () => {
+  return (
+    <div className="w-full">
+      <h2 className="text-center text-2xl mt-8 mb-2">Zamówienia</h2>
+      <Table />
+    </div>
+  )
+}
+
+export default Orders
