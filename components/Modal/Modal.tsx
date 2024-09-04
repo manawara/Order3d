390c646bd472783ead34121526d3f7a1d9b434c1
mnawara <marcinnawara@int.pl>
2024-09-04 23:27:33 +0200

    Refactor Modal

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,23 +1,29 @@
-'use client'
-import { forwardRef, useImperativeHandle, useRef } from 'react'
-import { ModalRef, ModalTypeProps } from '@/types/Modal.type'
+"use client";
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { ModalRef, ModalTypeProps } from "@/types/Modal.type";
+import { X } from "lucide-react";
 
 const Modal = forwardRef<ModalRef, ModalTypeProps>(({ children }, ref) => {
-  const dialogRef = useRef<HTMLDialogElement>(null)
+  const dialogRef = useRef<HTMLDialogElement>(null);
 
   useImperativeHandle(ref, () => ({
     open: () => dialogRef.current?.showModal(),
     close: () => dialogRef.current?.close(),
-  }))
+  }));
 
   return (
-    <dialog ref={dialogRef}>
-      <button onClick={() => dialogRef.current?.close()}>close</button>
+    <dialog
+      ref={dialogRef}
+      className="fixed pb-2 px-8 w-[90%] sm:w-2/3 md:w-1/2 bg-greenDark border-greenLight text-greenLight border backdrop:backdrop-blur-sm"
+    >
+      <button onClick={() => dialogRef.current?.close()}>
+        <X className="absolute top-1 right-1" size={20} />
+      </button>
       {children}
     </dialog>
-  )
-})
+  );
+});
 
-Modal.displayName = 'Modal'
+Modal.displayName = "Modal";
 
-export default Modal
+export default Modal;
