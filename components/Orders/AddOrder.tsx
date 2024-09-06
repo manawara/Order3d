03f9b41ebf03b6f7437b1a9814cab7e16d614c24
mnawara <marcinnawara@int.pl>
2024-09-06 08:54:27 +0200

    Create new schema for order

--- components/Orders/AddOrder.tsx
+++ components/Orders/AddOrder.tsx
@@ -1,60 +1,28 @@
-import Button from "../Button/Button";
-import Input from "../Input/Input";
-import Select from "../Select/Select";
+import { z } from "zod"; // Make sure to import 'z' from 'zod'
+import { Role } from "@/types/User.type";
+import { getGroupUsers } from "@/action/user";
+import FormAddOrder from "./FormAddOrder";
+import { useSession } from "next-auth/react";
+
+// Define your schema using Zod
+const orderSchema = z.object({
+  productName: z.string().nonempty("Nazwa produktu jest wymagana"),
+  quantity: z.number().min(1, "Ilość musi być większa niż 0"),
+  status: z.string().nonempty("Status jest wymagany"),
+  client: z.string().nonempty("Klient jest wymagany"),
+  price: z.number().positive("Cena musi być większa niż 0"),
+});
 
 const AddOrder = () => {
+  const { data: session } = useSession();
+
   return (
     <div className="flex flex-col w-full">
       <h3 className="text-center text-xl">Dodaj nowe zamówienie</h3>
       <p className="text-center mt-2">
         Wypełnij formularz, aby utworzyć nowe zamówienie.
       </p>
-      <form>
-        <Input label="Nazwa produktu" placeholder="Produkt 1" />
-        <Input label="Ilość" placeholder="0" type="number" />
-        <Select
-          name="status"
-          data={[
-            {
-              id: 1,
-              value: "Do zrobienia",
-            },
-            {
-              id: 2,
-              value: "W toku",
-            },
-            {
-              id: 3,
-              value: "Zrobione",
-            },
-          ]}
-          label="Status"
-          placeholder="Wybierz status"
-        />
-        <Select
-          name="status"
-          data={[
-            {
-              id: 1,
-              value: "Do zrobienia",
-            },
-            {
-              id: 2,
-              value: "W toku",
-            },
-            {
-              id: 3,
-              value: "Zrobione",
-            },
-          ]}
-          label="Klient"
-          placeholder="Wybierz Klienta"
-        />
-        <Input label="Cena" placeholder="np. xx zł" type="number" />
-        <div className="my-4">
-          <Button>Dodaj zamówienie</Button>
-        </div>
-      </form>
+      <FormAddOrder />
     </div>
   );
 };
