56ebfb6580a13f6990fabd2728cdfa3538cf3e35
mnawara <marcinnawara@int.pl>
2024-09-06 08:54:05 +0200

    New create function getGroupUsers

--- action/user.ts
+++ action/user.ts
@@ -1,48 +1,57 @@
-'use server'
-import { registerSchema } from '@/schema'
-import * as z from 'zod'
-import bcrypt from 'bcryptjs'
-import { db } from '@/lib/db'
-import { addNewUser } from './register'
+"use server";
+import { registerSchema } from "@/schema";
+import * as z from "zod";
+import bcrypt from "bcryptjs";
+import { db } from "@/lib/db";
+import { addNewUser } from "./register";
+import { Role } from "@prisma/client";
 
 export const getUserByEmail = async (email: string) => {
   return await db.user.findUnique({
     where: {
       email,
     },
-  })
-}
+  });
+};
+
+export const getGroupUsers = async (role: Role) => {
+  return await db.user.findMany({
+    where: {
+      role,
+    },
+  });
+};
 
 export const getTokenByEmail = async (email: string) => {
   return await db.verificationToken.findFirst({
     where: {
       email,
     },
-  })
-}
+  });
+};
 
 export const deleteToken = async (token: string) => {
   return await db.verificationToken.delete({
     where: { token },
-  })
-}
+  });
+};
 export const createUser = async (values: z.infer<typeof registerSchema>) => {
-  const validatedFields = registerSchema.safeParse(values)
+  const validatedFields = registerSchema.safeParse(values);
   if (validatedFields.success) {
-    const { email, name, password } = validatedFields.data
-    const existingUser = await getUserByEmail(email)
+    const { email, name, password } = validatedFields.data;
+    const existingUser = await getUserByEmail(email);
     if (existingUser) {
-      return { error: 'Konto o podanym emailu istnieje!' }
+      return { error: "Konto o podanym emailu istnieje!" };
     }
-    const hashedPassword = await bcrypt.hash(password, 10)
-    await addNewUser(name, email, hashedPassword)
-    return { success: 'Konto zostało stworzone!' }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await addNewUser(name, email, hashedPassword);
+    return { success: "Konto zostało stworzone!" };
   }
-  return null
-}
+  return null;
+};
 
 export const updatePassword = async (email: string, password: string) => {
-  const hashedPassword = await bcrypt.hash(password, 10)
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   await db.user.update({
     data: {
@@ -51,5 +60,5 @@ export const updatePassword = async (email: string, password: string) => {
     where: {
       email,
     },
-  })
-}
+  });
+};
