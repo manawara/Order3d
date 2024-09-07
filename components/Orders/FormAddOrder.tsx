f6379a3914b412c7200aec407a23e8e97ed10fbc
mnawara <marcinnawara@int.pl>
2024-09-08 00:00:39 +0200

    Invalidate queries after send order

--- components/Orders/FormAddOrder.tsx
+++ components/Orders/FormAddOrder.tsx
@@ -2,7 +2,7 @@
 import { useForm, Controller } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import Button from "../Button/Button";
 import Input from "../Input/Input";
 import Select from "../Select/Select";
@@ -13,10 +13,13 @@ import { ErrorMessage } from "@hookform/error-message";
 import { addNewOrder } from "@/action/order";
 
 const FormAddOrder = () => {
+  const queryClient = useQueryClient();
+
   const { data: users } = useQuery({
     queryKey: ["users", Role.USER],
     queryFn: () => getGroupUsers(Role.USER),
   });
+
   const userData =
     users?.map(({ name, id, email }) => ({
       id,
@@ -40,6 +43,8 @@ const FormAddOrder = () => {
   const onSubmit = (data: z.infer<typeof addOrder>) => {
     const clientEmail = data.client.match(/<(.+?)>/)?.[1];
     addNewOrder({ ...data, clientEmail });
+
+    queryClient.invalidateQueries({ queryKey: ["orders"] });
     reset();
   };
 
