fdf88c50403a167a782154d96195aab4767f67be
mnawara <marcinnawara@int.pl>
2024-09-07 23:59:42 +0200

    Create Loader and add to loading fetch orders

---
+++ components/Loader/Loader.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const loaderVariants = {
+  animationOne: {},
+};
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center mt-10">
+      <motion.div
+        className="w-16 h-16 border-t-4 border-greenLight border-solid rounded-full"
+        animate={{ rotate: 360 }}
+        transition={{ duration: 1, repeat: Infinity, ease: "linear" }}
+      />
+    </div>
+  );
+};
+
+export default Loader;
--- components/Orders/Orders.tsx
+++ components/Orders/Orders.tsx
@@ -1,12 +1,24 @@
+"use client";
 import React from "react";
 import Table from "../Table/Table";
 import { getOrders } from "@/action/order";
-import { getUserByEmail, getUserByID } from "@/action/user";
 import { formatDate } from "@/helpers";
 import { statusOrder } from "@/schema";
-const Orders = async () => {
-  const orders = await getOrders();
-  const ordersData = orders.map(({ id, name, createdAt, status, user }) => ({
+import { useQuery } from "@tanstack/react-query";
+import Loader from "../Loader/Loader";
+import { useContextPagination } from "@/context/PaginationContext";
+
+const Orders = () => {
+  const { pageNumber } = useContextPagination();
+  console.log(pageNumber);
+  const { data, isLoading } = useQuery({
+    queryKey: ["orders", pageNumber],
+    queryFn: async () => await getOrders(pageNumber, 20),
+  });
+  const orders = data?.orders;
+  const totalCount = data?.totalCount as number;
+
+  const ordersData = orders?.map(({ id, name, createdAt, status, user }) => ({
     id,
     name,
     user: user.name,
@@ -19,10 +31,20 @@ const Orders = async () => {
         ? statusOrder.IN_PROGRESS
         : statusOrder.DONE,
   }));
+
   return (
     <div className="w-full">
       <h2 className="text-center text-2xl mt-8 mb-2">Zamówienia</h2>
-      <Table data={ordersData} />
+      {isLoading && <Loader />}
+      {ordersData && (
+        <Table
+          data={ordersData}
+          optionsPagination={{
+            postsPerPage: 20, // orders of page
+            countData: totalCount, // count orders
+          }}
+        />
+      )}
     </div>
   );
 };
