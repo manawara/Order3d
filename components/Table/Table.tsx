ec508487e592bc59c38d27e92dbce7e629db4e97
mnawara <marcinnawara@int.pl>
2024-09-09 17:50:02 +0200

    Delete dataHeader with Table

--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -6,16 +6,8 @@ import DropDownDetails from "../DropDownDetails/DropDownDetails";
 import { TableRowProps } from "@/types/Table.type";
 import Pagination from "../Pagination/Pagination";
 import { PaginationType } from "@/types/Pagination.type";
-const dataHeader = {
-  id: "ID",
-  name: "Nazwa towaru",
-  customer: "Nazwa klienta",
-  date: "Data utworzenia",
-  status: "Status",
-  action: "Akcja",
-};
 
-const Table = ({ data, optionsPagination }: TableRowProps) => {
+const Table = ({ data, optionsPagination, dataHeader }: TableRowProps) => {
   return (
     <div className="overflow-x-auto w-full text-greenLight  text-sm p-2">
       <table className="min-w-[600px]  sm:min-w-[800px] lg:min-w-full  overflow-x-auto overflow-y-scroll">
