dde764e91c61e1d32a649b6d3e4c322000a7e86d
mnawara <marcinnawara@int.pl>
2024-09-17 22:46:55 +0200

    Refactor EditPage

--- app/dashboard/orders/edit/[id]/page.tsx
+++ app/dashboard/orders/edit/[id]/page.tsx
@@ -3,19 +3,34 @@ import { getGroupUsers } from "@/action/user";
 import FormEdit from "@/components/Orders/Edit/FormEdit";
 import React from "react";
 
-const EditOrderID = async ({ params }: { params: { id: number } }) => {
-  const id = params.id;
-  const users = await getGroupUsers("USER");
-  const order = await getOrderByID(id);
+interface Params {
+  params: {
+    id: string;
+  };
+}
 
-  return (
-    <div className="mt-8">
-      <h2 className="text-center text-2xl font-bold">
-        Edycja zamówienia nr: #{id}
-      </h2>
-      {order && users && <FormEdit order={order} users={users} />}
-    </div>
-  );
+const EditOrderID = async ({ params }: Params) => {
+  const id = parseInt(params.id, 10); // Ensure `id` is a number
+  try {
+    const users = await getGroupUsers("USER");
+    const order = await getOrderByID(id);
+
+    return (
+      <div className="mt-8">
+        <h2 className="text-center text-2xl font-bold">
+          Edycja zamówienia nr: #{id}
+        </h2>
+        {order && users ? (
+          <FormEdit order={order} users={users} />
+        ) : (
+          <p>No data available</p>
+        )}
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching data", error);
+    return <p className="text-center">Error loading order or users data</p>;
+  }
 };
 
 export default EditOrderID;
